6c0a64969f13986b0bd04808f4e03cdef0482ae0
Frwops <franciscosotoreyes4@gmail.com>
2024-07-17 00:37:18 -0400

    version estable con la portada lista

--- lib/estilo/paleta_colores.dart
+++ lib/estilo/paleta_colores.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color backgroundColor = Color.fromARGB(255, 0, 0, 0);
   static const Color textColor = Color.fromARGB(255, 255, 255, 255);
-
+  static const Color underlined = Color.fromARGB(255, 255, 0, 0);
   // Agrega más colores según sea necesario
 }
--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
                       ),
                     ),
                     Container(
-                      padding: const EdgeInsets.all(16),
+                      padding: EdgeInsets.fromLTRB(30.sp, 20, 30.sp, 0),
                       child: Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
@@ -52,7 +52,7 @@ class MyApp extends StatelessWidget {
                                   TextSpan(
                                     text: 'Francisco',
                                     style: TextStyle(
-                                      color: Colors.red,
+                                      color: AppColors.underlined,
                                       fontWeight: FontWeight.w800,
                                     ),
                                   ),
@@ -69,34 +69,41 @@ class MyApp extends StatelessWidget {
                                   10.w), // Espacio entre el texto y la imagen
                           Image.asset(
                             'assets/images/portfolio.jpg',
-                            width: MediaQuery.of(context).size.width / 3,
+                            width: MediaQuery.of(context).size.width / 5,
                             fit: BoxFit.cover,
                           ),
                           SizedBox(
                               width: 10
                                   .w), // Espacio entre la imagen y el siguiente texto
                           Expanded(
-                              child: RichText(
-                            textAlign: TextAlign.center,
-                            text: TextSpan(
-                                style: TextStyle(
-                                  color: AppColors.textColor,
-                                  fontSize: 5.sp,
-                                  fontFamily: 'Inter',
-                                ),
-                                children: const [
-                                  TextSpan(text: "“Mi curiosidad y "),
-                                  TextSpan(
-                                    text: "amor ",
+                              child: Column(
+                            children: [
+                              SizedBox(
+                                height: 90.sp,
+                              ),
+                              RichText(
+                                textAlign: TextAlign.center,
+                                text: TextSpan(
                                     style: TextStyle(
-                                      color: Colors.red,
-                                      fontWeight: FontWeight.w800,
+                                      color: AppColors.textColor,
+                                      fontSize: 5.sp,
+                                      fontFamily: 'Inter',
                                     ),
-                                  ),
-                                  TextSpan(
-                                      text:
-                                          "por aprender me\nimpulsan a explorar constantemente\nnuevas tecnologías y conceptos.”")
-                                ]),
+                                    children: const [
+                                      TextSpan(text: "“Mi curiosidad y "),
+                                      TextSpan(
+                                        text: "amor ",
+                                        style: TextStyle(
+                                          color: AppColors.underlined,
+                                          fontWeight: FontWeight.w800,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                          text:
+                                              "por aprender me\nimpulsan a explorar constantemente\nnuevas tecnologías y conceptos.”")
+                                    ]),
+                              ),
+                            ],
                           )),
                         ],
                       ),
