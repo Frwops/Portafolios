1765714610a96436d0a3a7181234c1816a6230b3
Frwops <franciscosotoreyes4@gmail.com>
2024-07-16 22:16:41 -0400

    version estable con la portada lista pero sin ordenar

---
+++ lib/estilo/paleta_colores.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundColor = Color.fromARGB(255, 0, 0, 0);
+  static const Color textColor = Color.fromARGB(255, 255, 255, 255);
+
+  // Agrega más colores según sea necesario
+}
